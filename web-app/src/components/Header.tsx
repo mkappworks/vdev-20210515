aa7b56ee701fae87e04889aabee47d439c704184
Malith Kuruppu <info.mkappworks@gmail.com>
2021-05-17 00:55:53 +0530

    add the Header.tsx

---
+++ web-app/src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="text-lg leading-6 font-medium text-black">Movies</div>
+      <form className="relative">
+        <svg
+          width="20"
+          height="20"
+          fill="currentColor"
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          />
+        </svg>
+        <input
+          className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10"
+          type="text"
+          aria-label="LookUp a Movie"
+          placeholder="LookUp a Movie"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Header;
