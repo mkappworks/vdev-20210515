78160be4001f3aa030be8bad1a40dbd9d85f4edd
Malith Kuruppu <info.mkappworks@gmail.com>
2021-05-16 20:12:28 +0530

    added api/movies get route

---
+++ backend-app/src/routes/MoviesRoute.ts
@@ -0,0 +1,30 @@
+import { FastifyPluginAsync } from "fastify";
+
+const moviesData = [
+  {
+    id: 1,
+    name: "Inception",
+    year: 2010,
+    description: "A thief tries to plant an idea into the mind of a C.E.O.",
+  },
+  {
+    id: 2,
+    name: "The Matrix",
+    year: 1999,
+    description: "A hacker discovers a shocking truth about his world.",
+  },
+  {
+    id: 3,
+    name: "Donnie Darko",
+    year: 2001,
+    description: "A troubled teenager follows a man in a rabbit suit.",
+  },
+];
+
+const moviesRoute: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.get("/movies", async function () {
+    return moviesData;
+  });
+};
+
+export default moviesRoute;
