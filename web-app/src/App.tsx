e50e24c374ff2a85c0b0f6ae36d0656c64124e82
Malith Kuruppu <info.mkappworks@gmail.com>
2021-05-17 13:52:53 +0530

    included store in App.tsx

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import useStore from "./store/store";
 
 import Header from "./components/Header";
 import MovieItemList from "./components/MovieItemList";
-import MovieItem from "./components/MovieItem";
 import MovieCounter from "./components/MovieCounter";
 
 const App = () => {
-  const list = [<MovieItem />, <MovieItem />, <MovieItem />];
+  const fetchMovies = useStore((state) => state.fetchMovies);
+
+  useEffect(() => {
+    fetchMovies("http://localhost:3000/api/movies");
+  }, []);
+
+  const movies = useStore((state) => state.movies);
+
+  const movieCount = movies.length;
+
+  let movieTable = <p>Found no Movies!</p>;
+
+  if (movies.length > 0) {
+    movieTable = <MovieItemList movies={movies}></MovieItemList>;
+  }
 
   return (
     <div className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4">
       <Header />
-      <MovieItemList>{list}</MovieItemList>
-      <MovieCounter count={3} />
+      {movieTable}
+      <MovieCounter count={movieCount} />
     </div>
   );
 };
